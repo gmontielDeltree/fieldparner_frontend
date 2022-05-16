235a03d56913ac14a4abc37d0834617b455ae7aa
LGO <oaky_oaky2004@yahoo.com.ar>
2022-05-16 12:56:19 -0300

    Manual Chucks

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
+import { visualizer } from "rollup-plugin-visualizer";
 export default {
     // config options para pouch
     optimizeDeps: {
         allowNodeBuiltins: ['pouchdb-browser', 'pouchdb-utils']
-    }
+    },
+    
+    plugins: [visualizer()],
+    build: {
+        rollupOptions: {
+          // https://rollupjs.org/guide/en/#big-list-of-options
+          manualChunks: {
+            pdfmake: ['pdfmake','node_modules/pdfmake/build/vfs_fonts.js'],
+
+            "agrotools_logo_b64.js": ['./owncomponents/lote-offcanvas/agrotools_logo_b64.js']
+          }
+        }
+      }
   }
