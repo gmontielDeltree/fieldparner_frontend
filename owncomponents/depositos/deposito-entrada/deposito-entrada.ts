9bfd89037da91f08180bc404c357e7b12caa7d36
LGO <oaky_oaky2004@yahoo.com.ar>
2022-06-09 19:15:05 -0300

    Componente Deposito Entrada

---
+++ owncomponents/depositos/deposito-entrada/deposito-entrada.ts
@@ -0,0 +1,26 @@
+import { LitElement, html } from "lit-element";
+
+export class DepositoEntrada extends LitElement {
+  
+  static properties = {
+    
+  };
+
+  constructor() {
+    super();
+  }
+
+  //createRenderRoot() {
+  //  return this;
+  //}
+
+  firstUpdated() {
+
+  }
+
+  render() {
+    return html``;
+  }
+}
+
+customElements.define("deposito-entrada", DepositoEntrada);
