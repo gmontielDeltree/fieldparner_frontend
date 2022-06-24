5f316c49ddf95f7752ea23729c528e45935c9382
Lgo <lgoharriz@gmail.com>
2022-06-24 17:38:58 -0300

    Localizar Lote

--- owncomponents/lote-offcanvas/lote-offcanvas.js
+++ owncomponents/lote-offcanvas/lote-offcanvas.js
@@ -34,6 +34,7 @@ import "lit-flatpickr";
 import "./cosecha-add-ui.js";
 import "./siembra-add-ui.js";
 import { google_maps_link_go_to } from "./google_maps.js";
+import bbox from "@turf/bbox";
 
 const capitalize = (mySentence) => {
   if (mySentence === null || mySentence === undefined) {
@@ -543,6 +544,10 @@ export class LoteOffcanvas extends LitElement {
       });
   }
 
+  localizar_lote(){
+    this.map.fitBounds(bbox(this._lote_doc),{padding: {top:50, bottom:window.innerHeight/2}})
+  }
+
   render() {
     //console.log("RENDER LOTE OFFCANVAS");
 
@@ -603,6 +608,13 @@ export class LoteOffcanvas extends LitElement {
         data-bs-backdrop="false"
       >
         <div class="offcanvas-header py-2">
+          <button type="button" class="btn btn-primary btn-sm" @click=${this.localizar_lote}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg> <span class='d-none d-md-inline'>Localizar</span>
+</button>
+
           <button
             class="btn btn-danger btn-sm d-block d-md-none material-icons md-18"
             @click=${this.eliminar_lote}
