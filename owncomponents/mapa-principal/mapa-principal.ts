2cb723b43be413cac30e7a303203a2837975f31c
Lgo <lgoharriz@gmail.com>
2022-07-28 01:12:08 -0300

    Mapa Satelital Uncomment

--- owncomponents/mapa-principal/mapa-principal.ts
+++ owncomponents/mapa-principal/mapa-principal.ts
@@ -153,8 +153,8 @@ export class MapaPrincipal extends LitElement {
   firstUpdated() {
     this.map = new Map({
       container: this.shadowRoot.getElementById("map"),
-      style: "mapbox://styles/mapbox/outdoors-v11",
-      //style: "mapbox://styles/mapbox/satellite-streets-v11?optimize=true",
+      //style: "mapbox://styles/mapbox/outdoors-v11",
+      style: "mapbox://styles/mapbox/satellite-streets-v11?optimize=true",
       center: [-59.2965, -35.1923],
       zoom: 12,
       attributionControl: true,
