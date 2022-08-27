0332c2933ff9f1eecad9d78ff9d1067b3eb9de92
Lgo <lgoharriz@gmail.com>
2022-08-27 15:22:39 -0300

    Comentarios

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-//import "mapbox-gl/dist/mapbox-gl.css";
-//import "bootstrap/dist/css/bootstrap.min.css";
-//import "material-icons/iconfont/material-icons.css";
 import "./owncomponents/field-partner/field-partner.ts";
-
-//import "bootstrap";
