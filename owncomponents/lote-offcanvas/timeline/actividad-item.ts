90f2e599d829acf07817a6ba5679aecbe82401e2
Lgo <lgoharriz@gmail.com>
2022-12-18 00:43:55 -0300

    Badge y Menu

--- owncomponents/lote-offcanvas/timeline/actividad-item.ts
+++ owncomponents/lote-offcanvas/timeline/actividad-item.ts
@@ -9,25 +9,76 @@ import "@vaadin/icon";
 import "@vaadin/icons";
 import "@vaadin/button";
 import "@vaadin/details";
-import "@vaadin/horizontal-layout"
-import '@vaadin/upload';
+import "@vaadin/horizontal-layout";
+import "@vaadin/upload";
+import "@vaadin/menu-bar";
+import {badge} from "@vaadin/vaadin-lumo-styles/badge"
 
 @customElement("actividad-item")
 export class ActividadItem extends LitElement {
+  
+  static override styles = [badge]
   @property()
   item: Actividad;
 
+  private menu_items = [
+    {
+      component: this.createItem("ellipsis-dots-v"),
+      tooltip: "Mas",
+      children: [
+        {
+          text: "Editar",
+          tooltip: "Edit",
+        },
+        {
+          text: "Orden de Trabajo PDF",
+          tooltip: "Move",
+        },
+        {
+          text: "Compartir Orden de Trabajo",
+          tooltip: "Duplicate",
+        },
+        {
+          text: "Datos Meteorológicos",
+          tooltip: "Archive",
+        },
+        {
+          text: "Eliminar",
+          tooltip: "Archive",
+        },
+      ],
+    },
+  ];
+
+  createItem(iconName: string) {
+    const item = document.createElement("vaadin-context-menu-item");
+    const icon = document.createElement("vaadin-icon");
+    icon.setAttribute("icon", `vaadin:${iconName}`);
+    item.appendChild(icon);
+    return item;
+  }
+
   render() {
-    return html` <div>
-        <a>${this.item?.tipo.toUpperCase()}</a>
-        <span class="text-muted">en ${this.item.detalles.hectareas} has.</span>
-        <vaadin-button
-          @click=${() => Router.go(url_repeticion(this.item._id))}
-          theme="icon"
-          aria-label="Mas"
-          ><vaadin-icon icon="vaadin:ellipsis-dots-h"></vaadin-icon>
-        </vaadin-button>
+    return html`
+    
+    <vaadin-horizontal-layout
+        theme=""
+        style="align-items:center; justify-content:space-between;"
+      >
+      <div><span theme="badge success">2022-10-31</span>
+      <a
+          >${this.item?.tipo.toUpperCase()}
+          <span class="text-muted">
+            en ${this.item.detalles.hectareas} has.</span
+          ></a
+        >
       </div>
+
+
+        <vaadin-menu-bar .items="${this.menu_items}" theme="icon">
+          <vaadin-tooltip slot="tooltip"></vaadin-tooltip>
+        </vaadin-menu-bar>
+      </vaadin-horizontal-layout>
       <vaadin-tabsheet>
         <vaadin-tabs slot="tabs">
           <vaadin-tab id="dashboard-tab">Planificación</vaadin-tab>
@@ -37,7 +88,7 @@ export class ActividadItem extends LitElement {
 
         <!-- Planificacion -->
         <div tab="dashboard-tab">
-            <div>Fecha Estimada de Aplicación</div>
+          <div>Fecha Estimada de Aplicación</div>
           <vaadin-details opened theme="small">
             <div slot="summary">Insumos</div>
 
@@ -54,6 +105,20 @@ export class ActividadItem extends LitElement {
           </vaadin-details>
           <vaadin-details theme="small">
             <div slot="summary">Contratista</div>
+
+            <ul>
+              <li>Blake Martin</li>
+              <li>Caroline Clark</li>
+              <li>Avery Torres</li>
+              <li>Khloe Scott</li>
+              <li>Camila Fisher</li>
+              <li>Gavin Lewis</li>
+              <li>Isabella Powell</li>
+              <li>Zoe Wilson</li>
+            </ul>
+          </vaadin-details>
+          <vaadin-details theme="small">
+            <div slot="summary">Condiciones de Trabajo</div>
 
             <ul>
               <li>Blake Martin</li>
@@ -85,16 +150,25 @@ export class ActividadItem extends LitElement {
 
         <!-- Ejecucion -->
         <div tab="payment-tab">
-        <vaadin-horizontal-layout theme="spacing padding" style="justify-content: center">
-            <vaadin-button @click=${()=>Router.go(gbl_state.router.urlForPath('/ejecucion'))} theme="primary success">
-                Ejecutar
+          <vaadin-horizontal-layout
+            theme="spacing padding"
+            style="justify-content: center"
+          >
+            <vaadin-button
+              @click=${() =>
+                Router.go(gbl_state.router.urlForPath("/ejecucion"))}
+              theme="primary success"
+            >
+              Ejecutar
             </vaadin-button>
-        </vaadin-horizontal-layout>
+          </vaadin-horizontal-layout>
         </div>
         <!-- Fin Ejecucion -->
 
         <!-- Adjuntos -->
-        <div tab="shipping-tab">This is the Shipping tab content</div>
+        <div tab="shipping-tab">
+          <vaadin-upload target="/api/fileupload"></vaadin-upload>
+        </div>
         <!-- FinAdjuntos-->
       </vaadin-tabsheet>`;
   }
