f44d21aa038391275a64ca33700307fff5f25a5f
LGO <oaky_oaky2004@yahoo.com.ar>
2023-01-02 22:07:12 -0300

    Movimiento de Jsones a carpeta propia. Reduccion.

---
+++ owncomponents/lote-offcanvas/timeline/detalles-actividad/detalles-ejecucion.ts
@@ -0,0 +1,53 @@
+import { html } from "lit";
+import { Actividad, Ejecucion, LineaDosisEjecucion } from "../../../depositos/depositos-types";
+import { map } from "lit/directives/map.js";
+
+const ejecucion_detalles = (ejecucion : Ejecucion, actividad: Actividad) => html`
+
+   <vaadin-details opened theme="small">
+                  <div slot="summary">Insumos</div>
+
+                  <ul>
+                    ${map(
+                      ejecucion.detalles.dosis,
+                      (item : LineaDosisEjecucion) =>
+                        html`<li>
+                          ${item.insumo.marca_comercial} -
+                          ${item.dosis.toFixed(3)} ${item.insumo.unidad}/ha
+                        </li>`
+                    )}
+                  </ul>
+                </vaadin-details>
+                <vaadin-details theme="small">
+                  <div slot="summary">Contratista</div>
+                  <ul>
+                    <li>${actividad.contratista.nombre}</li>
+                  </ul>
+                </vaadin-details>
+                <vaadin-details theme="small">
+                  <div slot="summary">Condiciones de Trabajo</div>
+                  <ul>
+                    <li>
+                      Temperatura promedio:
+                      ${ejecucion.condiciones.temperatura_promedio}
+                    </li>
+                    <li>
+                      Humedad promedio:
+                      ${ejecucion.condiciones.humedad_promedio}
+                    </li>
+                    <li>
+                      Velocidad promedio:
+                      ${ejecucion.condiciones.velocidad_promedio}
+                    </li>
+                  </ul>
+                </vaadin-details>
+                <vaadin-details theme="small">
+                  <div slot="summary">Observaciones</div>
+                  <ul>
+                    <li>${ejecucion.comentario}</li>
+                  </ul>
+                </vaadin-details>
+
+`
+
+export {ejecucion_detalles}
