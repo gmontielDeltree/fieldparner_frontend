cb602b98c7062860c88b133f64046a1595bc3f86
LGO <oaky_oaky2004@yahoo.com.ar>
2023-01-28 08:11:27 -0300

    Routes como archivo separado

---
+++ owncomponents/routes.ts
@@ -0,0 +1,67 @@
+export const routes = [
+    { path: "/", component: "null-component" },
+    { path: "/gf", redirect: "/" },
+    { path: "/campos", component: "lista-de-campos" },
+    { path: "/indices/:uuid", component: "ndvi-offcanvas" },
+    { path: "/cultivos", component: "color-cultivo" },
+    {
+      path: "/campo/:uuid_campo/lote/:uuid_lote/siembra/add",
+      component: "lote-offcanvas-side",
+    },
+    {
+      path: "/campo/:uuid_campo/lote/:uuid_lote/siembra/edit",
+      component: "lote-offcanvas-side",
+    },
+    {
+      path: "/campo/:uuid_campo/lote/:uuid_lote",
+      component: "lote-offcanvas-side",
+    },
+    {
+      path: "/campo/:uuid_campo/lote/:uuid_lote/actividad/:uuid_actividad/repetir",
+      component: "repetir-aplicacion",
+    },
+    {
+      path: "/campo/:uuid_campo/lote/:uuid_lote/actividad/nueva/:tipo",
+      component: "upsert-aplicacion",
+    },
+    {
+      path: "/campo/:uuid_campo/lote/:uuid_lote/actividad/editar/:uuid",
+      component: "upsert-aplicacion",
+    },
+    {
+      path: "/campo/:uuid_campo/lote/:uuid_lote/ejecucion/:uuid/nueva",
+      component: "upsert-ejecucion",
+    },
+    {
+      path: "/campo/:uuid_campo/lote/:uuid_lote/ejecucion/:uuid/editar",
+      component: "upsert-ejecucion",
+    },
+    { path: "/campo/add", component: "nuevo-campo" },
+    { path: "/campo/:uuid", component: "campo-offcanvas" },
+    { path: "/contratistas", component: "contratistas-lista" },
+    { path: "/contratistas/add", component: "contratistas-crud" },
+    { path: "/depositos", component: "depositos-listado" },
+    { path: "/depositos/add", component: "depositos-upsert" },
+    {
+      path: "/deposito/:uuid",
+      
+      children: [
+        {
+          path:'/',
+          component: "deposito-detalles",
+        },
+        {
+          path: "/transfer/add/:direccion",
+          component: "deposito-nuevo-transferencias",
+        },
+      ],
+    },
+    { path: "/insumos", component: "insumos-lista" },
+    { path: "/rights/:uuid_workspace", component: "workspace-rights" },
+    { path: "/invite/:base64_invitation", component: "link-invitacion" },
+    {
+      path: "/device/:uuid/dashboard/:date",
+      component: "device-route-handler",
+    },
+    { path: "/ejecucion", component: "null" },
+  ]
