dd032bbd78d047e0d750153b83bf2117a56796a3
LGO <oaky_oaky2004@yahoo.com.ar>
2023-08-01 22:19:12 -0300

    Route

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -10,7 +10,7 @@ export const AppRouter: React.FC = () => {
     const router = createBrowserRouter([
         ...old_routes, // old_routes es la parte "vieja" de la app
         // { path: "/auth/*", element: <>PublicRoute</> }, //Rutas publicas
-        // { path: "/overview/*", element: <OverviewRoutes /> } //Rutas privadasb
+     { path: "/overview/*", element: <OverviewRoutes /> } //Rutas privadasb
     ]);
 
     return <RouterProvider router={router} />
