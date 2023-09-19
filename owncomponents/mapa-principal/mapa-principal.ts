1a119541e0cd2dfdcfe87d862fde0987dec467f5
Lgo <lgoharriz@gmail.com>
2023-09-18 23:27:18 -0300

    color popup

--- owncomponents/mapa-principal/mapa-principal.ts
+++ owncomponents/mapa-principal/mapa-principal.ts
@@ -244,6 +244,17 @@ export class MapaPrincipal extends LitElement {
       .icon-dual {
         background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools --%3E%3Csvg width='29px' height='29px' viewBox='0 0 15 15' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM7.49988 1.82689C4.36688 1.8269 1.82707 4.36672 1.82707 7.49972C1.82707 10.6327 4.36688 13.1725 7.49988 13.1726V1.82689Z' fill='%23000000' /%3E%3C/svg%3E");
       }
+      
+      .mapboxgl-popup-content {
+        background-color: #053B50;
+        color: #EEEEEE;
+        
+      }
+
+      .mapboxgl-popup-anchor-left .mapboxgl-popup-tip {
+        border-right-color: #053B50;
+      }
+
     `,
   ];
 
