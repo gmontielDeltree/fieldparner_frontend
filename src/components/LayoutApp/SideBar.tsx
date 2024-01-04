3ffffd8e81c0099f5d39ab09716dba880f648ff1
German Montiel <german_montiel96@hotmail.com>
2024-01-03 21:44:57 -0300

    menu y submenus en sidebar

--- src/components/LayoutApp/SideBar.tsx
+++ src/components/LayoutApp/SideBar.tsx
@@ -1,6 +1,7 @@
 import { Link as RouterLink, useLocation } from "react-router-dom";
 import {
   Box,
+  Collapse,
   Divider,
   Drawer,
   IconButton,
@@ -26,20 +27,31 @@ import {
   Transform as TransformIcon,
   Agriculture as AgricultureIcon,
   AddLocationAlt as AddLocationAltIcon,
+  ExpandLess as ExpandLessIcon,
+  ExpandMore as ExpandMoreIcon,
 } from "@mui/icons-material";
 import { SideBarProps } from "../../types";
 import { Typography } from "@mui/material";
+import { useState } from "react";
+
+const keysCollapse = ["seguridad", "configuracion", "maestros", "agricultura", "stock", "cosecha", "gestion", "reporting", "wiki", "erp"];
+
+//TODO: crear objeto de menu y submenus
 
 export const SideBar: React.FC<SideBarProps> = ({
   drawerWidth,
   open,
   handleSideBarClose
 }) => {
+  const [openCollapse, setOpenCollapse] = useState('');
+
   const { pathname } = useLocation();
   const version = "2.23.1";
   const navigateTo = (path: string) => {
     window.location.replace(path);
   };
+
+  const onClickMenu = (collapse: string) => setOpenCollapse(collapse === openCollapse ? "" : collapse);
 
   return (
     <Box
@@ -69,6 +81,60 @@ export const SideBar: React.FC<SideBarProps> = ({
         </Box>
         <Divider />
         <List>
+          <ListItem key="Seguridad" disablePadding>
+            <ListItemButton
+              // component={RouterLink}
+              // to="/init/overview/fields"
+              onClick={() => onClickMenu(keysCollapse[0])}
+            // selected={pathname.includes("/init/overview/fields")}
+            >
+              <ListItemIcon>
+                <CabinIcon />
+              </ListItemIcon>
+              <ListItemText primary="Seguridad" />
+              {openCollapse === keysCollapse[0] ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            </ListItemButton>
+          </ListItem>
+          <Collapse key={keysCollapse[0]}
+            in={openCollapse === keysCollapse[0]}
+            timeout="auto"
+            unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <CabinIcon />
+                </ListItemIcon>
+                <ListItemText primary="Usuarios y permisos" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+          <ListItem key="Configuracion" disablePadding>
+            <ListItemButton
+              // component={RouterLink}
+              // to="/init/overview/fields"
+              onClick={() => onClickMenu(keysCollapse[1])}
+            // selected={pathname.includes("/init/overview/fields")}
+            >
+              <ListItemIcon>
+                <CabinIcon />
+              </ListItemIcon>
+              <ListItemText primary="Configuracion" />
+              {openCollapse === keysCollapse[1] ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            </ListItemButton>
+          </ListItem>
+          <Collapse key={keysCollapse[1]}
+            in={openCollapse === keysCollapse[1]}
+            timeout="auto"
+            unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <CabinIcon />
+                </ListItemIcon>
+                <ListItemText primary="Definicion Campaña" />
+              </ListItemButton>
+            </List>
+          </Collapse>
           <ListItem key="campos" disablePadding>
             <ListItemButton
               component={RouterLink}
@@ -121,7 +187,7 @@ export const SideBar: React.FC<SideBarProps> = ({
               <ListItemText primary="Salida de Campo" />
             </ListItemButton>
           </ListItem>
-          <ListItem key= "origins-destinations" disablePadding>
+          <ListItem key="origins-destinations" disablePadding>
             <ListItemButton
               component={RouterLink}
               to="/init/overview/origins-destinations"
