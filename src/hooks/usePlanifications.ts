a31ec2deff903b5de64a20b711f5fd217b968e0c
LGO <oaky_oaky2004@yahoo.com.ar>
2024-01-17 20:34:23 -0300

    InitPlan

---
+++ src/hooks/usePlanifications.ts
@@ -0,0 +1,82 @@
+import Swal from "sweetalert2";
+import { useState } from "react";
+import { dbContext } from "../services";
+import { useAppSelector } from ".";
+import { IPlanificacion } from "../interfaces/planification";
+
+export const usePlanification = () => {
+  const { user } = useAppSelector((state) => state.auth);
+  const [planifications, setPlanifications] = useState<IPlanificacion[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  let db = dbContext.fields as unknown as PouchDB.Database<IPlanificacion>;
+
+  const getPlanifications = async () => {
+    setIsLoading(true);
+    try {
+      const result = await db.find({
+        selector: { accountId: user?.accountId },
+      });
+
+      setIsLoading(false);
+      if (result.docs) {
+        const documents: IPlanificacion[] = result.docs.map(
+          (row) => row as IPlanificacion
+        );
+        setPlanifications(documents);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.error("Error al cargar documentos:", error);
+    }
+  };
+
+
+
+  const putPlanification = async (updateDeposit: IPlanificacion) => {
+    setIsLoading(true);
+    try {
+      const response = await db.put(updateDeposit);
+      setIsLoading(false);
+
+      if (response.ok) {
+        Swal.fire("Deposito", "Actualizado con exito.", "success");
+      }
+
+    } catch (error) {
+      console.log("Error al actualizar el documento: ", error);
+      Swal.fire("Ups", "Ocurrio un error inesperado ", "error");
+      setIsLoading(false);
+    }
+  };
+
+  const deletePlanification = async (deletePlanificationId: string) => {
+    setIsLoading(true);
+    try {
+      const doc = await db.get(deletePlanificationId);
+
+      const response = await db.remove(doc);
+      setIsLoading(false);
+
+      if (response.ok) Swal.fire("Plan", "Eliminado.", "success");
+    } catch (error) {
+      console.log("Error al actualizar el documento: ", error);
+      Swal.fire("Ups", "Ocurrio un error inesperado ", "error");
+      setIsLoading(false);
+    }
+  };
+
+  // const removeSupply = async () => {
+
+  // }
+
+  return {
+    planifications,
+    isLoading,
+
+    setPlanifications,
+    getPlanifications,
+    putPlanification,
+    deletePlanification,
+  };
+};
--- src/pages/PlanificationPage.tsx
+++ src/pages/PlanificationPage.tsx
@@ -1,9 +1,97 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { ActividadEditorBase } from "../components/Planification/ActividadEditorBase";
+import {
+  IActividadPlanificacion,
+  ICiclosPlanificacion,
+  IPlanificacion,
+  TTipoActividadPlanificada,
+} from "../interfaces/planification";
+import { uuidv7 } from "uuidv7";
+import { formatISO } from "date-fns";
+import { usePlanification } from "../hooks/usePlanifications";
 
-export const PlanificationPage : React.FC = ( )=>{
+export const PlanificationPage: React.FC = () => {
+  const { planifications, getPlanifications, putPlanification } =
+    usePlanification();
 
-    return (<>
-        
-        <div>TODO Planification</div>
-    </>)
-}
+  useEffect(() => {
+    getPlanifications();
+  }, []);
+  
+  useEffect(() => {
+    console.log("planificaciones", planifications);
+  }, [planifications]);
+
+  let actDoc: IActividadPlanificacion = {
+    accountId: "ffdfs",
+    _id: "plan:actividad:" + uuidv7(),
+    insumos: [],
+    labores: [],
+    fecha: formatISO(new Date()),
+    tipo: TTipoActividadPlanificada.COSECHA,
+    area: 23.4,
+    totalCosto: 2344,
+    campanaId: "dddd",
+    planId: "dddd",
+    cicloId: "cicloid",
+    campoId: "campoId",
+    loteId: "loteId",
+    ejecutada: false,
+    created: { userId: "dfsdfd", date: "" },
+    modified: { userId: "dfsdfd", date: "" },
+  };
+
+  let ciclo: ICiclosPlanificacion = {
+    _id: "plan:ciclo:" + uuidv7(),
+    accountId: "ffdss",
+    campanaId: "campañaid",
+    fechaInicio: formatISO(new Date()),
+    fechaFin: formatISO(new Date()),
+    actividades: [actDoc],
+    planId: "dddd",
+    campoId: "ddd",
+    loteId: "ssssss",
+    costoTotal: 2122,
+    cultivoId: "sojaId",
+    created: {
+      userId: "dfsdf",
+      date: "",
+    },
+    modified: {
+      userId: "fdfsd",
+      date: "",
+    },
+  };
+
+  let plan: IPlanificacion = {
+    ciclos: [ciclo],
+    campanaId: "",
+    locked: false,
+    accountId: "",
+    created: {
+      userId: "",
+      date: "",
+    },
+    modified: {
+      userId: "",
+      date: "",
+    },
+    _id: "plan:"+uuidv7(),
+  };
+
+  return (
+    <>
+      <div>
+        <ActividadEditorBase
+          actividadDoc={plan.ciclos[0].actividades[0]}
+          onSave={() => {
+            // Update doc
+            console.log("SAVE ACTIVIDAD TODO");
+            putPlanification(plan);
+            getPlanifications();
+          }}
+        ></ActividadEditorBase>
+      </div>
+    </>
+  );
+};
