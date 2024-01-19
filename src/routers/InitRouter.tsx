fca439a0b7d7861414e82b0a76f3a5275f8a8c43
LGO <oaky_oaky2004@yahoo.com.ar>
2024-01-18 21:52:18 -0300

    Test Redirect

--- src/routers/InitRouter.tsx
+++ src/routers/InitRouter.tsx
@@ -3,13 +3,24 @@ import { useAuthStore } from "../hooks";
 import { PublicRoutes } from "./PublicRoutes";
 import { OverviewRoutes } from "./OverviewRoutes";
 import { Loading } from "../components";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const InitRouter = () => {
+  let navigate = useNavigate();
+  let location = useLocation();
+
   const { status, checkAuthToken } = useAuthStore();
 
   useEffect(() => {
     checkAuthToken();
   }, []);
+
+  useEffect(() => {
+    if(status === "authenticated"){
+      navigate(-1);
+    }
+  }, [status]);
+
 
   if (status === "checking") {
     return <Loading key="loading-auth" loading />;
