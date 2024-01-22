ca9a6fadcc5c9c4811099b97eb448fa2d25ac20d
LGO <oaky_oaky2004@yahoo.com.ar>
2024-01-22 08:56:18 -0300

    Nav Impovements

--- src/routers/InitRouter.tsx
+++ src/routers/InitRouter.tsx
@@ -8,26 +8,34 @@ import { useLocation, useNavigate } from "react-router-dom";
 export const InitRouter = () => {
   let navigate = useNavigate();
   let location = useLocation();
-  const [from, setFrom] = useState()
+
+
+  // Original URL
+  const [from, setFrom] = useState<string>("/" + (window.location.pathname+window.location.search).substr(1))
 
   const { status, checkAuthToken } = useAuthStore();
 
   useEffect(() => {
+    // anotar la url original
+    console.log("WINDOW LOCATION HREF PRE AUTH", from)
     checkAuthToken();
+
   }, []);
 
   
-  // useEffect(() => {
-  //   if(status === "authenticated"){
-  //     // Despues de autenticar hay que volver a -2 
-  //     // para recuperar la url antes del login. Ej.:
-  //     // 1 - /init/overview/deposits
-  //     // 2 - /login (por checkAuthToken)
-  //     // 3 - /init/overview/fields (por el render de mas abajo)
-  //     // 4 - navigate(-2) vuelve a /init/overview/deposits 
-  //     navigate(-2);
-  //   }
-  // }, [status]);
+  useEffect(() => {
+    if(status === "authenticated"){
+      // Despues de autenticar hay que volver atras
+      // para recuperar la url antes del login. Ej.:
+      // 1 - /init/overview/deposits (request original)
+      // 2 - /login (por checkAuthToken)
+      // 3 - /init/overview/fields (por el render de mas abajo)
+      // 4 - navigate(from) vuelve a /init/overview/deposits
+      
+      console.log("WINDOW LOCATION HREF POST AUTH", window.location.href) 
+      navigate(from);
+    }
+  }, [status]);
 
 
   if (status === "checking") {
@@ -36,7 +44,7 @@ export const InitRouter = () => {
 
   return (
     <>
-      {status === "not-authenticated" ? <PublicRoutes /> : <OverviewRoutes />}
+      {status !== "not-authenticated" ? <PublicRoutes /> : <OverviewRoutes />}
     </>
   );
 };
