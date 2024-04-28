be54428664e247f6288220f16a510de6356e4f26
mbusnelli34 <106454340+mbusnelli34@users.noreply.github.com>
2024-04-28 15:11:54 -0300

    Update Activity.tsx

--- src/components/LotsMenu/Activities/Activity.tsx
+++ src/components/LotsMenu/Activities/Activity.tsx
@@ -23,8 +23,7 @@ function Activity({
   handleConfirmExecution
 }) {
   const [gradientAngle, setGradientAngle] = useState(0);
-  const [showReplicateActivityMenu, setShowReplicateActivityMenu] =
-    useState(false);
+  const [showReplicateActivityMenu, setShowReplicateActivityMenu] = useState(false);
 
   const executionDate = useMemo(() => {
     if (
@@ -34,24 +33,21 @@ function Activity({
       return new Date(activity.actividad.detalles.fecha_ejecucion_tentativa);
     }
     return new Date();
-  }, [activity.actividad.detalles.fecha_ejecucion_tentativa]);
+  }, [activity]);
 
   useEffect(() => {
-    const newDate = new Date(
+    if (
+      activity &&
+      activity.actividad &&
+      activity.actividad.detalles &&
       activity.actividad.detalles.fecha_ejecucion_tentativa
-    );
-    if (newDate.getTime() !== executionDate.getTime()) {
-      setExecutionDate(newDate);
+    ) {
+      const newDate = new Date(activity.actividad.detalles.fecha_ejecucion_tentativa);
+      if (newDate.getTime() !== executionDate.getTime()) {
+        setExecutionDate(newDate);
+      }
     }
-  }, [activity.fecha_ejecucion]);
-
-  // LANZA DEMASIADOS RENDERIZADOS
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setGradientAngle((prevAngle) => (prevAngle + 1) % 360);
-  //   }, 100);
-  //   return () => clearInterval(interval);
-  // }, []);
+  }, [activity]);
 
   const gradientBackground = `linear-gradient(${gradientAngle}deg, ${complementaryColor} 30%, #f0f0f0 100%)`;
 
@@ -69,7 +65,7 @@ function Activity({
   };
 
   const renderActivityContent = () => {
-    switch (activity.actividad.tipo) {
+    switch (activity?.actividad?.tipo) {
       case "preparado":
         return (
           <Preparation
@@ -157,14 +153,12 @@ function Activity({
   };
 
   return (
-    <div
-      style={{ display: "flex", marginBottom: "32px", position: "relative" }}
-    >
+    <div style={{ display: "flex", marginBottom: "32px", position: "relative" }}>
       <div style={{ marginRight: "8px", position: "relative", zIndex: 2 }}>
         {icon && (
           <img
             src={icon}
-            alt={activity.actividad.tipo}
+            alt={activity?.actividad?.tipo}
             style={{
               height: "40px",
               width: "40px",
@@ -197,7 +191,7 @@ function Activity({
         <CardContent>
           {showReplicateActivityMenu ? (
             <ReplicateActivityMenu
-              originalActivity={activity.actividad}
+              originalActivity={activity?.actividad}
               handleReplicateActivity={handleReplicateActivity}
             />
           ) : (
@@ -213,3 +207,4 @@ function Activity({
 }
 
 export default React.memo(Activity);
+
